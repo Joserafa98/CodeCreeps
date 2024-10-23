023558baad3d378f29f2614cb788f929fd4b8fd3
Jose Hernandez <JOSERAFAHERNANDEZ.98@GMAIL.COM>
2024-10-23 19:05:53 +0000

    making the swiper

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/home.css";
+import "../../styles/home.css";;
 import Swiper from 'swiper';
 
 export const Home = () => {
@@ -22,142 +22,145 @@ useEffect(() => {
 		  <>
 			{/* HEADER */}
 			<header className="header" id="header">
-			  <nav className="nav container">
-				<a href="#" className="nav__logo"> CodeCreeps </a>
-				<div className="nav__menu" id="nav-menu">
-				  <ul className="nav__list">
-					<li className="nav__item">
-					  <a href="#home" className="nav__link active-link">Home</a>
-					</li>
-					<li className="nav__item">
-					  <a href="#trick" className="nav__link">Trick</a>
-					</li>
-					<li className="nav__item">
-					  <a href="#treat" className="nav__link">Treat</a>
-					</li>
-					<a href="#" className="button button--ghost">ÚNETE AHORA</a>
-				  </ul>
-				  <div className="nav__close" id="nav-close">
-					<i className='bx bx-x'></i>
-				  </div>
-				  <img src="https://assets.codepen.io/7773162/nav-img.png" alt="" className="nav__img" />
-				</div>
-				<div className="nav__toggle" id="nav-toggle">
-				  <i className='bx bx-grid-alt'></i>
-				</div>
-			  </nav>
-			</header>
-			<main className="main">
-			  {/* HOME */}
-			  <section className="home container" id="home">
-				<div className="swiper home-swiper">
-				  <div className="swiper-wrapper">
-					{/* HOME SLIDER 1 */}
-					<section className="swiper-slide">
-					  <div className="home__content grid">
-						<div className="home__group">
-						  <img src="https://assets.codepen.io/7773162/home3-img.png" alt="" className="home__img" />
-						  <div className="home__indicator"></div>
-						  <div className="home__details-img">
-							<h4 className="home__details-title">Captain Sem</h4>
-							<span className="home__details-subtitle">Veteran Spooky Ghost</span>
-						  </div>
+				<nav className="nav container">
+					<a href="#" className="nav__logo"> CodeCreeps </a>
+					<div className="nav__menu" id="nav-menu">
+					<ul className="nav__list">
+						<li className="nav__item">
+						<a href="#home" className="nav__link active-link">Home</a>
+						</li>
+						<li className="nav__item">
+						<a href="#trick" className="nav__link">Trick</a>
+						</li>
+						<li className="nav__item">
+						<a href="#treat" className="nav__link">Treat</a>
+						</li>
+						<a href="#" className="button button--ghost">ÚNETE AHORA</a>
+					</ul>
+					<div className="nav__close" id="nav-close">
+						<i className='bx bx-x'></i>
+					</div>
+					<img src="https://assets.codepen.io/7773162/nav-img.png" alt="" className="nav__img" />
+					</div>
+					<div className="nav__toggle" id="nav-toggle">
+					<i className='bx bx-grid-alt'></i>
+					</div>
+				</nav>
+				</header>
+
+				<main className="main">
+				{/* HOME */}
+				<section className="home container" id="home">
+					<div className="swiper home-swiper">
+					<div className="swiper-wrapper">
+						{/* HOME SLIDER 1 */}
+						<section className="swiper-slide">
+						<div className="home__content grid">
+							<div className="home__group">
+							<img src="https://assets.codepen.io/7773162/home3-img.png" alt="" className="home__img" />
+							<div className="home__indicator"></div>
+							<div className="home__details-img">
+								<h4 className="home__details-title">Los retos te están esperando</h4>
+								<span className="home__details-subtitle">¡Participa ahora!</span>
+							</div>
+							</div>
+							<div className="home__data">
+							<h3 className="home__subtitle">Reta tus habilidades mientras conjuras código</h3>
+							<h1 className="home__title">APRENDE Y <br /> JUEGA CON<br /> NOSOTROS </h1>
+							<p className="home__description">
+								En CodeCreeps, te invitamos a adentrarte en un oscuro mundo de desafíos de programación. 
+								Desbloquea tus habilidades mientras enfrentas retos escalofriantes y te diviertes creando hechizos de código. 
+								¡Prepárate para aprender y jugar en esta noche de brujas llena de sorpresas <strong>espeluznantes!</strong>
+							</p>
+							<div className="home__buttons">
+								<a href="#" className="book--now">
+								<img src="https://assets.codepen.io/7773162/svgviewer-output+%281%29_3.svg" alt="" />
+								</a>
+							</div>
+							</div>
 						</div>
-						<div className="home__data">
-						  <h3 className="home__subtitle">Reta tus habilidades mientras conjuras código</h3>
-						  <h1 className="home__title">APRENDE Y <br /> JUEGA CON<br />  NOSOTROS </h1>
-						  <p className="home__description">
-						  En CodeCreeps, te invitamos a adentrarte en un oscuro mundo de desafíos de programación. Desbloquea tus habilidades mientras enfrentas retos escalofriantes y te diviertes creando hechizos de código. ¡Prepárate para aprender y jugar en esta noche de brujas llena de sorpresas <strong>espeluznantes! </strong>
-						  </p>
-						  <div className="home__buttons">
-							<a href="#" className="book--now">
-							  <img src="https://assets.codepen.io/7773162/svgviewer-output+%281%29_3.svg" alt="" />
-							</a>
-							<a href="#" className="button--link button--flex">
-							  Track Record <i className='bx bx-right-arrow-alt button__icon'></i>
-							</a>
-						  </div>
+						</section>
+
+						{/* HOME SLIDER 2 */}
+						<section className="swiper-slide">
+						<div className="home__content grid">
+							<div className="home__group">
+							<img src="https://assets.codepen.io/7773162/home2-img.png" alt="" className="home__img" />
+							<div className="home__indicator"></div>
+							<div className="home__details-img">
+								<h4 className="home__details-title">Adino & Grahami</h4>
+								<span className="home__details-subtitle">No words can describe them</span>
+							</div>
+							</div>
+							<div className="home__data">
+							<h3 className="home__subtitle">Comparte con tus amigos y</h3>
+							<h1 className="home__title">JUNTOS <br /> DEL TOP <br /> MUNDIAL</h1>
+							<p className="home__description">
+								Los participantes mas #top saldrán en nuestra lista de los mejores
+							</p>
+							<div className="home__buttons">
+								<a href="#" className="book--now">
+								<img src="https://assets.codepen.io/7773162/svgviewer-output+%281%29_3.svg" alt="" />
+								</a>
+								<a href="#" className="button--link button--flex">
+								Track Record <i className='bx bx-right-arrow-alt button__icon'></i>
+								</a>
+							</div>
+							</div>
 						</div>
-					  </div>
-					</section>
-					{/* HOME SLIDER 2 */}
-					<section className="swiper-slide">
-					  <div className="home__content grid">
-						<div className="home__group">
-						  <img src="https://assets.codepen.io/7773162/home2-img.png" alt="" className="home__img" />
-						  <div className="home__indicator"></div>
-						  <div className="home__details-img">
-							<h4 className="home__details-title">Adino & Grahami</h4>
-							<span className="home__details-subtitle">No words can describe them</span>
-						  </div>
+						</section>
+
+						{/* HOME SLIDER 3 */}
+						<section className="swiper-slide">
+						<div className="home__content grid">
+							<div className="home__group">
+							<img src="https://assets.codepen.io/7773162/home1-img.png" alt="" className="home__img" />
+							<div className="home__indicator"></div>
+							<div className="home__details-img">
+								<h4 className="home__details-title">The Labu “Reiza”</h4>
+								<span className="home__details-subtitle">The Living Pumpkin</span>
+							</div>
+							</div>
+							<div className="home__data">
+							<h3 className="pumpkin__subtitle">#3 Top Scariest Ghost 2020</h3>
+							<h1 className="home__title">UOOOO <br /> TRICK OR <br /> TREAT!!</h1>
+							<p className="home__description">
+								Hi I’m Reiza, people call me “The Labu” currently I’m trying to learn something new, 
+								building my own bike with parts only made from Malaysia.
+							</p>
+							<div className="home__buttons">
+								<a href="#" className="book--now">
+								<img src="https://assets.codepen.io/7773162/svgviewer-output+%282%29_2.svg" alt="" />
+								</a>
+								<a href="#" className="button--link button--flex">
+								Track Record <i className='bx bx-right-arrow-alt button__icon'></i>
+								</a>
+							</div>
+							</div>
 						</div>
-						<div className="home__data">
-						  <h3 className="home__subtitle">#2 top Best duo</h3>
-						  <h1 className="home__title">BRING BACK <br /> MY COTTON <br /> CANDY </h1>
-						  <p className="home__description">
-							Adino steals cotton candy from his brother and eats them all in one bite, a hungry beast. Grahami can no longer contain his anger towards Adino.
-						  </p>
-						  <div className="home__buttons">
-							<a href="#" className="book--now">
-							  <img src="https://assets.codepen.io/7773162/svgviewer-output+%281%29_3.svg" alt="" />
-							</a>
-							<a href="#" className="button--link button--flex">
-							  Track Record <i className='bx bx-right-arrow-alt button__icon'></i>
-							</a>
-						  </div>
-						</div>
-					  </div>
-					</section>
-					{/* HOME SLIDER 3 */}
-					<section className="swiper-slide">
-					  <div className="home__content grid">
-						<div className="home__group">
-						  <img src="https://assets.codepen.io/7773162/home1-img.png" alt="" className="home__img" />
-						  <div className="home__indicator"></div>
-						  <div className="home__details-img">
-							<h4 className="home__details-title">The Labu “Reiza”</h4>
-							<span className="home__details-subtitle">The Living Pumpkin</span>
-						  </div>
-						</div>
-						<div className="home__data">
-						  <h3 className="pumpkin__subtitle">#3 Top Scariest Ghost 2020</h3>
-						  <h1 className="home__title">UOOOO <br /> TRICK OR <br /> TREAT!! </h1>
-						  <p className="home__description">
-							Hi I’m Reiza, people call me “The Labu” currently I’m trying to learn something new, building my own bike with parts only made from Malaysia.
-						  </p>
-						  <div className="home__buttons">
-							<a href="#" className="book--now">
-							  <img src="https://assets.codepen.io/7773162/svgviewer-output+%282%29_2.svg" alt="" />
-							</a>
-							<a href="#" className="button--link button--flex">
-							  Track Record <i className='bx bx-right-arrow-alt button__icon'></i>
-							</a>
-						  </div>
-						</div>
-					  </div>
-					</section>
-				  </div>
-				  <div className="swiper-pagination"></div>
-				</div>
-			  </section>
+						</section>
+					</div>
+					<div className="swiper-pagination"></div>
+					</div>
+				</section>
 			  {/* CATEGORY */}
 			  <section className="section category" id="trick">
-				<h2 className="section__title">Codea-te con <br /> <strong>Los mejores</strong> </h2>
+				<h2 className="section__title">Retos <br /> <strong>Espeluznantes</strong> <br /> Para todos los niveles </h2>
 				<div className="category__container container grid">
 				  <div className="category__data">
 					<img src="https://assets.codepen.io/7773162/category1-img.png" alt="" className="category__img" />
-					<h3 className="category__title">Ghosts</h3>
-					<p className="category__description">Choose the ghosts, the scariest there are.</p>
+					<h3 className="category__title">Fantasmas</h3>
+					<p className="category__description">Retos especiales para los que quieren una experiencia aterradora... BUU</p>
 				  </div>
 				  <div className="category__data">
 					<img src="https://assets.codepen.io/7773162/category2-img.png" alt="" className="category__img" />
-					<h3 className="category__title">Pumpkins</h3>
-					<p className="category__description">You look at the scariest pumpkins there is.</p>
+					<h3 className="category__title">Calabazas</h3>
+					<p className="category__description">¡Retos diseñados para los que quieren un primer susto!</p>
 				  </div>
 				  <div className="category__data">
 					<img src="https://assets.codepen.io/7773162/category3-img.png" alt="" className="category__img" />
-					<h3 className="category__title">Witch Hat</h3>
-					<p className="category__description">Pick the most stylish witch hats out there.</p>
+					<h3 className="category__title">Brujas</h3>
+					<p className="category__description">Retos tan mágicos que sacarán el developer que eres</p>
 				  </div>
 				</div>
 			  </section>
@@ -176,6 +179,7 @@ useEffect(() => {
 				  <img src="https://assets.codepen.io/7773162/about-img.png" alt="" className="about__img" />
 				</div>
 			  </section>
+
 			  {/* TRICK */}
 			  <section className="section trick">
 				<div className="trick__container container grid">
