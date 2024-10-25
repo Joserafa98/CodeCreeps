18c8d5605e7806990c4b3628795f7d55d9bf2ade
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-10-25 02:53:29 +0000

    ddtalles sobre nosotros

--- src/front/js/pages/aboutUs.js
+++ src/front/js/pages/aboutUs.js
@@ -93,7 +93,7 @@ export const AboutUs = () => {
                                 <h5 className="h5 mb-3 text-uppercase fw-bolder text-halloween-orange spooky-font">Cristian Ayala Sánchez</h5>
                                 <h2 className="ls-tight font-display text-halloween-orange fw-bolder mb-3 spooky-font">Full Stack Developer</h2>
                                 <p className="lead mb-5 spooky-font fade-in">
-                                    Soy Cristian Ayala Sánchez, un Full Stack Developer apasionado por la tecnología y la creación de soluciones web. Me encanta enfrentar desafíos y aprender constantemente nuevas habilidades en el desarrollo de software...
+                                Soy Cristian Ayala Sánchez, un Full Stack Developer apasionado por la tecnología y la creación de soluciones web. Me encanta enfrentar desafíos y aprender constantemente nuevas habilidades en el desarrollo de software. Siempre busco formas de mejorar mis conocimientos y de aplicar lo que aprendo en proyectos significativos.En mi tiempo libre, disfruto explorar nuevas tendencias en tecnología y colaborar con otros desarrolladores. Mi objetivo es seguir creciendo en mi carrera y contribuir a proyectos que impacten positivamente en la sociedad.
                                 </p>
                                 <div className="social-buttons d-flex mt-4">
                                     <a href="https://github.com/cristiann05" target="_blank" rel="noopener noreferrer" className="btn btn-halloween btn-lg mx-2">GitHub</a>
@@ -116,7 +116,7 @@ export const AboutUs = () => {
                             <div className="col-lg-5 mb-7 mb-lg-0">
                                 <h5 className="h5 mb-3 text-uppercase fw-bolder text-halloween-orange spooky-font">José Hernández</h5>
                                 <h2 className="ls-tight font-display text-halloween-orange fw-bolder mb-3 spooky-font">Full Stack Developer</h2>
-                                <p className="lead mb-5 spooky-font fade-in">Soy José Hernández, periodista y marketer egresado de la Universidad Arturo Michelena...</p>
+                                <p className="lead mb-5 spooky-font fade-in">Soy José Hernández, periodista y marketer egresado de la Universidad Arturo Michelena, con una pasión por el aprendizaje continuo y una sólida trayectoria en el desarrollo web. Actualmente Full Stack Developer, especializado en la creación de soluciones tecnológicas con un enfoque integral, combinando habilidades en front-end y back-end. Busco seguir perfeccionándome en el área de Inteligencia Artificial para aplicar sus innovaciones en el mundo digital.</p>
                                 <div className="social-buttons d-flex mt-4">
                                     <a href="https://github.com/Joserafa98" target="_blank" rel="noopener noreferrer" className="btn btn-halloween btn-lg mx-2">GitHub</a>
                                     <a href="https://www.linkedin.com/in/jose-hernandez-67605813b/" target="_blank" rel="noopener noreferrer" className="btn btn-halloween btn-lg mx-2">LinkedIn</a>
@@ -132,7 +132,8 @@ export const AboutUs = () => {
                             <div className="col-lg-5 mb-7 mb-lg-0">
                                 <h5 className="h5 mb-3 text-uppercase fw-bolder text-halloween-orange spooky-font">Beatriz Carmona Jurado</h5>
                                 <h2 className="ls-tight font-display text-halloween-orange fw-bolder mb-3 spooky-font">Full Stack Developer</h2>
-                                <p className="lead mb-5 spooky-font fade-in">Soy Beatriz Carmona, una entusiasta de la tecnología y el desarrollo de software...</p>
+                                <p className="lead mb-5 spooky-font fade-in">Soy Beatriz Carmona, una entusiasta de la tecnología y el desarrollo de software. Actualmente trabajo como Técnico de Operaciones en un Centro de Datos (DCO L4) en AWS, Dublín. Me encanta aprender y enfrentar nuevos desafíos cada día, lo que me ha llevado a inscribirme en un Bootcamp de Full Stack Software Developer en 4Geeks Academy.
+                                Mis principales fortalezas son el trabajo en equipo, la resolución de problemas y mi capacidad autodidacta. Busco seguir creciendo como desarrolladora y contribuir a proyectos con impacto positivo.</p>
                                 <div className="social-buttons d-flex mt-4">
                                     <a href="https://github.com/Ruubia" target="_blank" rel="noopener noreferrer" className="btn btn-halloween btn-lg mx-2">GitHub</a>
                                     <a href="https://www.linkedin.com/in/beatrizcarmonajurado" target="_blank" rel="noopener noreferrer" className="btn btn-halloween btn-lg mx-2">LinkedIn</a>
