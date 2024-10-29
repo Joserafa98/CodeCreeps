cb3fca95ba9f996be6ec7bf47881f22034036cdd
Cristian Ayala Sánchez <ayalasanchezcristian2000@gmail.com>
2024-10-29 02:26:23 +0000

    proyecto terminado definitivamente

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -10,7 +10,6 @@ const Navbar = () => {
     const location = useLocation();
     const { currentUser } = store; // Obtén el usuario actual del store
 
-    // Función para manejar el cierre de sesión
     const handleLogout = async () => {
         await actions.logoutUser(); // Llama a la acción de logout
         navigate('/'); // Redirige a la página principal después del logout
@@ -49,30 +48,30 @@ const Navbar = () => {
                             </li>
                             <li className="nav__item">
                                 <Link 
+                                    to="/minijuegos" 
+                                    className={`nav__link ${location.pathname === '/minijuegos' ? 'active-link' : ''}`}
+                                >
+                                    Mini Juegos
+                                </Link>
+                            </li>
+                            <li className="nav__item">
+                                <Link 
                                     to="/chatgeneral" 
                                     className={`nav__link ${location.pathname === '/chatgeneral' ? 'active-link' : ''}`}
                                 >
                                     Chat oscuro
                                 </Link>
                             </li>
-                            <li className="nav__item">
-                                <Link 
-                                    to="/miniJuegos" 
-                                    className={`nav__link ${location.pathname === '/miniJuegos' ? 'active-link' : ''}`}
-                                >
-                                    Mini Juegos
-                                </Link>
-                            </li>
                         </>
                     )}
-                    {currentUser ? ( // Si hay un usuario autenticado
+                    {currentUser ? (
                         <>
                             <Link to="/profile" className="nav__link">
                                 <img src={currentUser.profilePicture || ProfilePic} alt="Profile" className="profile-img" />
                             </Link>
                             <button onClick={handleLogout} className="button button--ghost" id='logout-button'>Cerrar sesión</button>
                         </>
-                    ) : ( // Si no hay usuario autenticado
+                    ) : (
                         <>
                             <Link to="/aboutUs" className="button button--ghost" id='user-button'>Sobre nosotros</Link>
                             <Link to="/signup" className="button button--ghost" id='user-button'>ÚNETE AHORA</Link>
